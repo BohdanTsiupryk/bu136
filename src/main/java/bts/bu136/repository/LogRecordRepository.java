06161c1d1a4acabc4eb71766446488a0c0f2c7e0
Bohdan Tsiupryk <bohdan.tsiupryk12@gmail.com>
2023-04-20 21:33:20 +0300

    add docker

--- src/main/java/bts/bu136/repository/LogRecordRepository.java
+++ src/main/java/bts/bu136/repository/LogRecordRepository.java
@@ -2,6 +2,8 @@ package bts.bu136.repository;
 
 import bts.bu136.model.LogRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LogRecordRepository extends JpaRepository<LogRecord, Long> {
 }
